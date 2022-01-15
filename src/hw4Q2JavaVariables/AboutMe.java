0fbe08f1c3253f81adb4733f016d44a7a48f4c4a
NabilaNHossain <nabilanhossain@gmail.com>
2022-01-15 15:55:52 -0600

    01152022 3:55pm

--- src/hw4Q2JavaVariables/AboutMe.java
+++ src/hw4Q2JavaVariables/AboutMe.java
@@ -1,37 +1,36 @@
 package hw4Q2JavaVariables;
 
 public class AboutMe {
-	//VARIABLES ARE DECLARED IN HERE//
+	// VARIABLES ARE DECLARED IN HERE//
 	public String name;
 	public byte age;
 	public short attendedDaysInEnthrallIt;
-	public int houseNumber ;
+	public int houseNumber;
 	public long carNumber;
 	public float height;
 	public double weight;
 	public char sex;
 	public boolean workInTheItIndustry;
+
 //CONSTRUCTOR IS DECLARED IN HERE//
 	public AboutMe() {
 		System.out.println("This is all about me. ");
 	}
+
 //METHOD IS IMPLEMENTED HERE//
 	public void aboutMe() {
-		System.out.println("Hi,my name is: "+ name + " .I am "+ age + " years old.\nI have been a student at EnthrallIT for " + attendedDaysInEnthrallIt + "days" + "\nI live at "+ houseNumber + " number home in Texas."
-				 + "\nMy car number is: "+ carNumber +" .\nI am " +  height + " Ft .\nI weigh " + weight + "lbs "+"\nI work in the IT industry: " + workInTheItIndustry +" .");
-		
+		System.out.println(
+				"Hi,my name is: " + name + " .I am " + age + " years old.\nI have been a student at EnthrallIT for "
+						+ attendedDaysInEnthrallIt + "days" + "\nI live at " + houseNumber + " number home in Texas."
+						+ "\nMy car number is: " + carNumber + " .\nI am " + height + " Ft .\nI weigh " + weight
+						+ "lbs " + "\nI work in the IT industry: " + workInTheItIndustry + " .");
+
 	}
+
 	public static void main(String[] args) {
 		AboutMe me = new AboutMe();
 		me.aboutMe();
 
-		
-	 
-		
-		
-		
 	}
-		
-		
 
 }
