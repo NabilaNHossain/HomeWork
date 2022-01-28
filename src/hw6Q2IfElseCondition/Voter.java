7316b73f6300ddc40b003e0701f308ad03f4ae22
NabilaNHossain <nabilanhossain@gmail.com>
2022-01-27 20:35:59 -0600

    01272022 8.35pm

--- src/hw6Q2IfElseCondition/Voter.java
+++ src/hw6Q2IfElseCondition/Voter.java
@@ -4,12 +4,11 @@ public class Voter {
 
 	public static void main(String[] args) {
 		int age = 33;
-		int age1 = 18;
-		if (age == age1) {
+		if (age == 18) {
 			System.out.println("I am a Voter");
-		} else if (age < age1) {
+		} else if (age < 18) {
 			System.out.println("I am not a Voter");
-		} else if (age >= age1) {
+		} else if (age >= 18) {
 			System.out.println("I am a Voter");
 		} else {
 			System.out.println("Please add a valid age");
