27bc33276c91461bae66fcea293d47d9a6da6ee1
NabilaNHossain <nabilanhossain@gmail.com>
2022-02-13 14:43:14 -0600

    02132022 2.43pm

--- src/hw8Q2Inheritance/Birds.java
+++ src/hw8Q2Inheritance/Birds.java
@@ -1,6 +1,5 @@
 package hw8Q2Inheritance;
 
-/*Birds extends Animal is an example of single inheritance. One derived class Birds inherits the properties and behavior of one single Base class, Animal*/
 public class Birds extends Animal {
 	public void birdsInfo() {
 		System.out.println("This method is from Birds Class");
